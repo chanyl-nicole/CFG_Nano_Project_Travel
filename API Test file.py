4c8ffe11e01f3409c1418d55c3731743d3937f13
Hockey321 <ameliabourton@gmail.com>
2021-08-01 19:37:08 +0100

    File to test accessing the travel perk api. currently experiencing an error - {"detail":"Authentication credentials were not provided."} so not sure if the api key we have is correct?

---
+++ API Test file.py
@@ -0,0 +1,21 @@
+import requests
+
+url = "https://api.travelperk.com/travelsafe/restrictions"
+
+querystring = {
+    "origin":"FR",
+    "destination":"ES",
+    "origin_type":"country_code",
+    "destination_type":"country_code",
+    "date":"2020-10-15"
+}
+
+headers = {
+    "Api-Version": "1",
+    "Authorization": "Api Key WAbFf2.zqU68TNEDYFWKkCQMZxif8gxq16A60qD"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
