ef747df00fe066a1f977e5bae9c570127d169302
Hockey321 <ameliabourton@gmail.com>
2021-08-08 15:59:12 +0100

    Travel advice api - need to use your own authorisation key.
    some example returned fields - issue with accessing origin = advice['trips']['from'] #can't get this dictionary to work

--- Travel Advice API test.py
+++ Travel Advice API test.py
@@ -1,12 +1,29 @@
 import requests
 
-url = "https://api.traveladviceapi.com/search/lhr:dxb"
+from_airport = input("Enter 3 digit code of airport flying from").upper()
+to_airport = input("Enter 3 digit code of airport flying to").upper()
+
+url = "https://api.traveladviceapi.com/search/{}:{}".format(from_airport,to_airport)
+
 
 payload={}
 headers = {
-  'X-Access-Token': 'ee538d1a-ffac-462e-94f3-1b3710691e29'
+  'X-Access-Token': '#insert own key here'
 }
+#ee538d1a-ffac-462e-94f3-1b3710691e29 #orginal key
 
 response = requests.request("GET", url, headers=headers, data=payload)
+advice = response.json()
 
-print(response.text)
+# origin = advice['trips']['from'] #can't get this dictionary to work
+# destination = advice['trips']['to']
+
+
+risk_level = advice['risk_level']
+requirements = advice['requirements']
+
+
+
+print("Risk level is {}".format(risk_level))
+print(requirements)
+print(origin)
--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import json
 
 url = "https://sandbox.travelperk.com/travelsafe/restrictions"
 
-querystring = {"origin": "DE",
-               "destination": "ES",
+origin_code= input("Enter 2 digit country code you are travelling from").upper() #this needs to be DE
+destination_code = input("Enter 2 digit country code you are travelling to").upper() #this needs to be ES
+
+querystring = {"origin": "{}".format(origin_code),
+               "destination": "{}".format(destination_code),
                "origin_type": "country_code",
                "destination_type": "country_code",
                "date": "2021-08-15"}
@@ -21,16 +24,20 @@ json_response = response.json()
 
 destination = json_response['destination']['name']
 origin = json_response['origin']['name']
-requirements = json_response["requirements"][0]["category"]["id"]
+travel_status = json_response['authorization_status']
+requirements = json_response['summary']
+quarantine = json_response['requirements'][0]['summary']
 documents = json_response['requirements'][1]['category']['name']
 to_fill = json_response['requirements'][1]['documents'][0]['document_url']
 
 
-print('Destination: ', destination)
 print('Origin: ', origin)
+print('Destination: ', destination)
+print('Travel Status: ',travel_status)
 print('Requirements: ', requirements)
-print('Documents: ', documents)
-print('Documents needed: ', to_fill)
+print('Quarantine: ', quarantine)
+# print('Documents: ', documents)
+print('Documents needed prior to arrival: ', to_fill)
 
 ###to see whole dictionary
 
