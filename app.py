882bfb8d2991ea2a1d51708a1cf34a0f0a3e898d
tdcalvo <t.diaz-calvo@uea.ac.uk>
2021-08-10 10:52:22 +0200

    files added to run travel app version 2 - under development

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+from db_utils import show_essential_items, show_months, show_cities, show_cities_and_weather, show_personal_items
 
 app = Flask(__name__)
 
-#our endpoint/routes go in here
+#  GET MONTHS FOR SUMMER HOLIDAYS
+    # get data from DB, put in json format, publish in the specified place (URL)
+    @app.route('/travel/<months>')  # generates the endpoint at an specific URl
+    def app_get_month_choices(): # better rename=  def get_months()
+        res = show_months()
+        return jsonify(res)
+
+#  GET TOP 8 EUROPEAN DESTINATIONS FOR SUMMER HOLIDAYS
+    @app.route('/travel/<cities>')
+    def app_get_cities():
+        res = show_cities()
+        return jsonify(res)
+
+#  GET WEATHER FOR TOP 8 EUROPEAN DESTINATIONS FOR SELECTED MONTH
+    @app.route('/travel/<cities-weather-month>')
+    def app_get_city_weather(month): # better rename=  def get_city_weather()
+        res = show_cities_and_weather(month)
+        return jsonify(res)
+
+#  POST LIST OF PERSONAL ITEMS NEEDED FOR TRIP -  not sure how to do this!
+
+    #add_user_personal_items(user_item)
+
+#  GET ESSENTIAL ITEMS FOR SELECTED MONTH AND DESTINATION
+    @app.route('/travel/<essential-items>')  # generates the endpoint at an specific URl
+    def app_get_essential_items(month, city):
+        res = show_essential_items(month, city)
+        return jsonify(res)
+
+#  GET PERSONAL ITEMS NEEDED FOR TRIP
+    @app.route('/travel/<personal-items>')
+    def app_get_personal_items(): # better rename=  def get_personal_items()
+        res = show_personal_items()
+        return jsonify(res)
+
+#  GET ALL ITEMS TO BRING TO OUR HOLIDAYS?
+
+#  GET COVID INFO FOR DESTINATION?
 
 
 if __name__ == '__main__':
---
+++ test_main_copy.py
@@ -0,0 +1,140 @@
+############## ALL THIS IS FROM THE CLIENT SIDE
+
+### this is code acting in the 'frontend'
+
+import requests # import requests library  = we can make requests to the API
+import json
+
+from db_utils import add_user_personal_items, remove_personal_items
+
+# def get_availability_by_date(date):  # request data for specific date
+#     result = requests.get(
+#         'http://127.0.0.1:5001/availability/{}'.format(date),
+#         headers={'content-type': 'application/json'}
+#     )
+#     return result.json()
+
+def get_month_choices():
+    result = requests.get(
+        #'http://127.0.0.1:5001/availability/{}'.format(date),
+        headers={'content-type': 'application/json'}
+    )
+    return result.json()
+
+def get_cities():
+    result = requests.get(
+        # 'http://127.0.0.1:5001/availability/{}'.format(date),
+        headers={'content-type': 'application/json'}
+    )
+    return result.json()
+
+def get_city_weather(month):
+    result = requests.get(
+        # 'http://127.0.0.1:5001/availability/{}'.format(date),
+        headers={'content-type': 'application/json'}
+    )
+    return result.json()
+
+def get_essential_items(month, city):
+    result = requests.get(
+        # 'http://127.0.0.1:5001/availability/{}'.format(date),
+        headers={'content-type': 'application/json'}
+    )
+    return result.json()
+
+def get_personal_items():
+    result = requests.get(
+        # 'http://127.0.0.1:5001/availability/{}'.format(date),
+        headers={'content-type': 'application/json'}
+    )
+    return result.json()
+
+
+def choose_month():
+    """ This function allows the user to choose a month for
+    their summer holidays and returns that month"""
+
+    print ("Are you ready to book your summer holiday? You can travel in the following months: ")
+    #month_choices = show_months()
+    month_choices = get_month_choices() # ??????????
+    print(month_choices)
+    month = input("What month would you like to travel in? ")
+# IMPORTANT for DEBUGGING AND TESTING:
+    # here, if input is not a valid month, should raise an Exception!!!!
+    # also, good case for testing what happens if we give the right or wrong input!
+    return month
+
+#choose_month()
+
+def choose_city(month):
+    """ This function allows the user to choose a city for
+    their summer holidays and returns that city"""
+
+    print("The weather on the top 8 european destinations for {} is the following: ".format(month))
+    #city_weather = show_cities_and_weather(month)
+    city_weather = get_city_weather(month)
+    for location in city_weather.keys():
+        print(location, ':', '', city_weather[location])
+    city = input("Which city would you like to go to: ")
+ # IMPORTANT for DEBUGGING AND TESTING:
+    # here, if input is not a valid city, should raise an Exception!!!!
+    # also, good case for testing what happens if we give the right or wrong input!
+    return city
+
+#choose_city(month)
+
+
+def add_personal_items():
+    """ This function allows the user to add personal items
+    needed for their holiday to the DB and returns a list of those items"""
+
+    print("You can save a remainder list of up to 10 personal items to bring to your trip")
+    user_items_list = []
+    counter = 0
+    while counter < 10:
+        user_item = input("Please, enter item to be saved on list or enter 'done' when done: ")
+        if user_item != 'done':
+            user_items_list=[]
+            # Good case for testing! What happens if input is 'done' or another word
+            if user_item not in user_items_list:
+                user_items_list.append(user_item)
+                add_user_personal_items(user_item)
+                counter += 1
+        else:
+            #print(user_items_list)
+            return user_items_list
+    #print(user_items_list)
+    return user_items_list
+
+#add_personal_items()
+
+def plan_your_trip():
+    """ This function allows the user to choose a month and city for their
+    summer holidays and to create a remainder list of personal items to bring to their trip
+    and returns these items plus suggested essential items for the chosen destination"""
+
+    month = choose_month()
+    city = choose_city(month)
+    add_items = add_personal_items()
+    #personal_items = show_personal_items()
+    personal_items = get_personal_items()
+    clear_items = remove_personal_items()
+    #essential_items = show_essential_items(month, city)
+    essential_items = get_essential_items(month, city)
+
+    print("These are the suggested essential items for your trip: ")
+    for i in essential_items:
+        print(i) # we may need to tidy up the output by stripping [0], which is '(' and [-2,-1], which are ',)' but each element (i) is a tuple!
+    print("These are your saved personal items to bring to your trip: ")
+    for i in personal_items:
+        print(i)
+
+    all_items = essential_items + personal_items
+
+    return all_items
+
+plan_your_trip() # Example input: Passport, Camera, Money, Asthma medication etc.
+
+##### IDEAS OF EXTRA FUNCTIONALITY IF WE HAD TIME FOR REPORT:
+    # In table of personal essential items in database, allow incorporating quantity of items needed
+    # Allow deleting items from list/table of personal items in case usr input one by mistake or decides it is not longer needed
